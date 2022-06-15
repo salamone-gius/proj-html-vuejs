43b2dfa7b801291d9281d9296b1b4e70eaa09750
salamone-gius <salamone.gius@gmail.com>
2022-06-15 22:15:24 +0200

    CommonButton comp created

---
+++ src/components/commons/CommonButton.vue
@@ -0,0 +1,52 @@
+<template>
+    <button :type="buttonType" :class="buttonClass">
+        <a :href="buttonLink">{{buttonText}}</a>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'CommonButton',
+
+    props: {
+        buttonType: String,
+        buttonClass: String,
+        buttonLink: String,
+        buttonText: String,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+    border-radius: 1.5rem;
+    padding: 0.8rem 2rem;
+    border: none;
+    cursor: pointer;
+    margin: 200px;
+
+    &:active {
+        transform: translateY(0.125rem);
+    }
+
+    &.primary {
+        background-color: var(--primary-button-bg);
+        color: var(--primary-button-txt);
+
+        &:hover {
+            background-color: var(--primary-button-hover-bg);
+            color: var(--primary-button-hover-txt);
+        }
+    }
+
+    &.secondary {
+        background-color: var(--secondary-button-bg);
+        color: var(--secondary-button-txt);
+    
+        &:hover {
+            background-color: var(--secondary-button-hover-bg);
+            color: var(--secondary-button-hover-txt);
+        }
+    }
+}
+</style>
--- src/components/footer/BaseFooter.vue
+++ src/components/footer/BaseFooter.vue
@@ -1,11 +1,19 @@
 <template>
     <footer>
+        <CommonButton :buttonText='"Learn more about us"' :buttonClass="'primary'"/>
+        <CommonButton :buttonText='"Learn more about us"' :buttonClass="'secondary'"/>
+
     </footer>
 </template>
 
 <script>
+import CommonButton from './../commons/CommonButton.vue'
 export default {
     name: 'BaseFooter',
+
+    components: {
+        CommonButton,
+    }
 }
 </script>
 
