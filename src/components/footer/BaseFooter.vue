039af8fcd6950d3faf35a8427305c3f10f3f47f9
salamone-gius <salamone.gius@gmail.com>
2022-06-15 22:37:33 +0200

    colors fixed

--- src/components/footer/BaseFooter.vue
+++ src/components/footer/BaseFooter.vue
@@ -1,18 +1,13 @@
 <template>
     <footer>
-        <CommonButton :buttonText='"Learn more about us"' :buttonClass="'primary'"/>
-        <CommonButton :buttonText='"Learn more about us"' :buttonClass="'secondary'"/>
-
     </footer>
 </template>
 
 <script>
-import CommonButton from './../commons/CommonButton.vue'
 export default {
     name: 'BaseFooter',
 
     components: {
-        CommonButton,
     }
 }
 </script>
