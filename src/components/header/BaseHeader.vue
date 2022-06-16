ce23df647967d68dbbc265b22e5707c9b6e17a61
salamone-gius <salamone.gius@gmail.com>
2022-06-16 09:22:47 +0200

    HeaderTop comp created

--- src/components/header/BaseHeader.vue
+++ src/components/header/BaseHeader.vue
@@ -1,12 +1,18 @@
 <template>
-    <header>
-
+    <header class="container">
+        <HeaderTop/>
     </header>
 </template>
 
 <script>
+import HeaderTop from './HeaderTop.vue';
+
 export default {
     name: 'BaseHeader',
+
+    components: {
+        HeaderTop,
+    }
 }
 </script>
 
---
+++ src/components/header/HeaderTop.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="header-top">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderTop',
+}
+</script>
+
+<style>
+
+</style>
