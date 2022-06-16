89656a2af7e7422f1f149123a8c61cc60b4253db
salamone-gius <salamone.gius@gmail.com>
2022-06-16 09:39:43 +0200

    LogoBox comp created and mounted on HeaderTop

---
+++ src/components/commons/LogoBox.vue
@@ -0,0 +1,18 @@
+<template>
+    <img :src="logoSrc" :alt="logoAlt">
+</template>
+
+<script>
+export default {
+    name: 'LogoBox',
+
+    props: {
+        logoSrc: String,
+        logoAlt: String,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/header/HeaderTop.vue
+++ src/components/header/HeaderTop.vue
@@ -1,15 +1,30 @@
 <template>
     <div class="header-top">
-
+        <LogoBox :logoSrc="logoInfo.logoImage" :logoAlt="logoInfo.logoImage"/>
     </div>
 </template>
 
 <script>
+import LogoBox from './../commons/LogoBox.vue';
+
 export default {
     name: 'HeaderTop',
+
+    components: {
+        LogoBox,
+    },
+
+    data() {
+        return {
+            logoInfo: {
+                logoImage: require('./../../assets/img/dark-pet-logo.png'),
+                logoAlt: 'Avada_logo',
+            }
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
