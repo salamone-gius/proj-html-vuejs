e2b96cef8050b117558a9627af1306d32978e430
salamone-gius <salamone.gius@gmail.com>
2022-06-17 15:59:54 +0200

    VerticalNavbar comp created

---
+++ src/components/commons/VerticalNavbar.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="vertical-navbar">
+        <h5>Shop by brand</h5>
+        <ul>
+            <li v-for="(category, index) in shopByBrand" :key="index">
+                <a :href="category.url">
+                    {{category.name}}
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VerticalNavbar',
+
+    data() {
+        return {
+            shopByBrand: [
+                {
+                    name: "Bed",
+                    url: "#"
+                },
+                {
+                    name: "Food",
+                    url: "#"
+                },
+                {
+                    name: "Toys",
+                    url: "#"
+                },
+                {
+                    name: "Transport",
+                    url: "#"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.vertical-navbar {
+    ul {
+        list-style: none;
+        padding: 0;
+
+        li {
+            padding: 0.8rem 0;
+
+            a {
+                text-decoration: none;
+                color: inherit;
+                cursor: pointer;
+
+                &:hover {
+                    color: var(--exanary-txt-color);
+                }
+            }
+        }
+    }
+}
+</style>
--- src/components/footer/FooterTop.vue
+++ src/components/footer/FooterTop.vue
@@ -4,18 +4,23 @@
             <div class="col">
                 <CompanyContacts/>
             </div>
+            <div class="col">
+                <VerticalNavbar/>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import CompanyContacts from './../commons/CompanyContacts.vue';
+import VerticalNavbar from './../commons/VerticalNavbar.vue';
 
 export default {
     name: 'FooterTop',
 
     components: {
         CompanyContacts,
+        VerticalNavbar,
     }
 }
 </script>
